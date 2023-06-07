b92d5d0136a6f8c59dd28ea5c71bd150b4e0ff57
Wendell Santos <wendellinacio@gmail.com>
2023-06-06 23:52:02 -0300

    Pagina 404

---
+++ resources/views/404.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.main')
+
+@section('content')
+    <main>
+        <div class="d-flex align-items-center justify-content-center" style="height: 500px;">
+            <div class="text-center">
+                <h5 class="display-1 fw-bold text-danger">Ops!</h5>
+                <p class="fs-3">Algo deu errado.</p>
+                <p class="lead">
+                    Esta página não existe ou o link está incorreto.
+                </p>
+            </div>
+        </div>
+    </main>
+@endsection
